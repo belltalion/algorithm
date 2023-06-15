ed7912a8da6af3915cc5f59a2646651426a31b9f
beltalion <xogus4026@gmail.com>
2023-06-15 14:54:30 +0900

    asdf

--- src/MartMap.java
+++ src/MartMap.java
@@ -3,36 +3,51 @@ import java.util.List;
 
 public class MartMap {
     private static final int INF = Integer.MAX_VALUE;
+
     public static void main(String[] args) {
-        int N = 8;  // 정점의 수
-        String[] places = { "마트입구", "생활 필수품", "가구점", "식료품점", "약국", "의류점", "전자제품점", "수산시장" };
+        int N = 8;
 
+        // 아이템 이름 배열
+        String[] items = {"마트입구", "전자레인지", "기저귀", "청소기", "돼지고기", "냉장고", "생수", "육개장 1박스", "의자"};
+        // 아이템 볼륨 배열
+        int[] volumes = {0, 10, 5, 25, 3, 40, 5, 15, 25};
+
+        // 그래프의 인접 행렬
         int[][] g = {
-                {0, 7, INF, INF, INF, INF, INF, INF},
-                {7, 0, 2, INF, INF, INF, INF, INF},
-                {INF, 2, 0, 4, INF, INF, INF, INF},
-                {INF, 2, 4, 0, 3, INF, INF, INF},
-                {INF, INF, 5, 3, 0, 5, INF, INF},
-                {INF, INF, INF, INF, 5, 0, 6, INF},
-                {INF, INF, INF, INF, INF, 6, 0, 7},
-                {INF, INF, INF, INF, INF, 10, 7, 0}
+                {0, 3, INF, 2, INF, INF, INF, INF},
+                {3, 0, 7, INF, 4, INF, INF, INF},
+                {INF, 7, 0, 5, INF, INF, INF, INF},
+                {2, INF, 5, 0, 7, INF, INF, INF},
+                {INF, 4, INF, 7, 0, 3, INF, INF},
+                {INF, INF, INF, INF, 3, 0, 2, INF},
+                {INF, INF, INF, INF, INF, 2, 0, 6},
+                {INF, INF, INF, INF, INF, INF, 6, 0}
         };
 
+        // 최소 신장 트리의 간선 정보
         int[][] mst = {
-                {0, 1, 7},
-                {1, 2, 2},
+                {0, 1, 3},
                 {1, 3, 7},
-                {3, 4, 3},
-                {4, 5, 5},
-                {5, 6, 6},
-                {6, 7, 7}
+                {3, 4, 7},
+                {4, 5, 3},
+                {5, 6, 2},
+                {6, 7, 6},
+                {1, 2, 7}
         };
 
+        int binSize = 40;
+        // 근사적인 Bin Packing 알고리즘을 사용하여 필요한 카트의 개수 계산
+        int binCount = approxBinPacking(volumes, binSize);
+
+        System.out.println("필요한 카트의 개수: " + binCount);
+
+        // 인접 리스트 생성
         List<List<Integer>> a = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             a.add(new ArrayList<>());
         }
 
+        // 그래프의 인접 리스트 표현과 가중치 정보 설정
         for (int i = 0; i < mst.length; i++) {
             int u = mst[i][0];
             int v = mst[i][1];
@@ -44,9 +59,10 @@ public class MartMap {
         }
 
         List<Integer> visitSeq = new ArrayList<>();
-        int current = 0; // 시작점을 마트입구(인덱스 0)로 변경
-        visitSeq.add(0); // 마트입구(인덱스 0)를 방문 순서에 추가
+        int current = 0;
+        visitSeq.add(0);
 
+        // DFS를 사용하여 지름길 방문 순서 계산
         while (!a.get(0).isEmpty()) {
             boolean flag = false;
             int k = 0;
@@ -68,17 +84,6 @@ public class MartMap {
             }
         }
 
-        visitSeq.add(0);
-
-        System.out.print("mst를 따른 방문 순서:\t");
-        for (int i = 0; i < visitSeq.size(); i++) {
-            System.out.print(places[visitSeq.get(i)]);
-            if (i != visitSeq.size() - 1) {
-                System.out.print(" <- ");
-            }
-        }
-        System.out.println();
-
         List<Integer> directSeq = new ArrayList<>();
         boolean[] mark = new boolean[N];
         for (int i = 0; i < visitSeq.size(); i++) {
@@ -88,15 +93,43 @@ public class MartMap {
             }
         }
 
-        System.out.print("지름길 방문 순서:\t");
-        for (int i = directSeq.size() - 1; i >= 0; i--) {
-            System.out.print(places[directSeq.get(i)]);
-            if (i != 0) {
-                System.out.print(" <- ");
+        System.out.print("지름길 방문 순서: ");
+        for (int i = 0; i < directSeq.size(); i++) {
+            System.out.print(items[directSeq.get(i)]);
+            if (i != directSeq.size() - 1) {
+                System.out.print(" -> ");
             }
         }
+        System.out.print(" -> 마트입구");
         System.out.println();
-
     }
 
+    // 근사적인 Bin Packing 알고리즘
+    public static int approxBinPacking(int[] itemVolumes, int binSize) {
+        List<Integer> bins = new ArrayList<>();
+        bins.add(binSize);
+
+        for (int i = 0; i < itemVolumes.length; i++) {
+            int itemVolume = itemVolumes[i];
+            int bestBinIndex = -1;
+            int bestRemainingSpace = Integer.MAX_VALUE;
+
+            for (int j = 0; j < bins.size(); j++) {
+                int remainingSpace = bins.get(j) - itemVolume;
+
+                if (remainingSpace >= 0 && remainingSpace < bestRemainingSpace) {
+                    bestBinIndex = j;
+                    bestRemainingSpace = remainingSpace;
+                }
+            }
+
+            if (bestBinIndex != -1) {
+                bins.set(bestBinIndex, bestRemainingSpace);
+            } else {
+                bins.add(binSize - itemVolume);
+            }
+        }
+
+        return bins.size();
+    }
 }
